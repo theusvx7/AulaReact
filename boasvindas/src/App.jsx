8c0662c235e65716576045bc98807befcfead845
YagoIsaque <yago.soares@etec.sp.gov.br>
2025-04-08 14:57:13 -0300

    0804

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,22 @@
+import BoasVindas from "./exemplo1"
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1> Boas vindas ao React com JavaScript</h1>
+    
+    <p>
+      Este é o arquivo app.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React utilizando JavaScript
+    </p>
+
+    <BoasVindas nome="Pingola" />
+    <BoasVindas nome="20comer 70correr" />
+    <BoasVindas nome="60ficaduru" />
+    <BoasVindas nome="Yago" />
+    </div>
+  )
+}
+
+export default App
